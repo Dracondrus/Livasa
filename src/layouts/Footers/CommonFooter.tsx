fa3cdd95a29482b1eec309a3eb23ef1d5026af9f
ELFASA <elfasatasa@gmail.com>
2025-07-10 08:16:21 +0500

    fixed bug 875nl982

--- src/layouts/Footers/CommonFooter.tsx
+++ src/layouts/Footers/CommonFooter.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { quickLinks, serviceLinks } from "@/data/footerLinks";
+import FooterContact from "./subComponents/FooterContact";
 
 import FooterCopyright from "./subComponents/FooterCopyright";
 
@@ -8,7 +10,14 @@ import FooterCopyright from "./subComponents/FooterCopyright";
         <footer className={`tp-footer-area p-relative ${className}`}>
             <div className="tp-footer-bg"></div>
             <div className="container">
-                
+                <div className="tp-footer-widget-border">
+                    <div className="row">
+                        <FooterContact />
+                        {/* <FooterColumn title="Quick Link" links={quickLinks} /> */}
+                        {/* <FooterColumn footerCol={3} title="Services" links={serviceLinks} /> */}
+                        {/* <FooterNewsletter /> */}
+                    </div>
+                </div>
                 <FooterCopyright />
             </div>
         </footer>
