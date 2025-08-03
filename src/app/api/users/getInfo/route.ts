89e24e678f671c750a55ac41f0ff47e378caab67
ELFASA <elfasatasa@gmail.com>
2025-08-04 00:38:53 +0500

    add profile4

--- src/app/api/users/getInfo/route.ts
+++ src/app/api/users/getInfo/route.ts
@@ -1,7 +1,7 @@
 import { sql } from "@/lib/db";
 import {  NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/options";
 
 
 export async function GET() {
